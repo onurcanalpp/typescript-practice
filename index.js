d05ee2f5ed11bd71f6c4a4f012e7c01510426189
onurcanalpp <onurcanalpp@gmail.com>
2021-07-13 13:35:15 +0300

    basic functions

---
+++ index.js
@@ -0,0 +1,23 @@
+// type userInfoTypes = name:string, surname:string, age:number;
+var userInfo = {
+    name: "Onur",
+    surname: "Alp",
+    age: 22,
+    gender: "Man"
+};
+var basicMath = function (x, y) { return x + y; };
+var result = basicMath(5, 5);
+var users = [
+    {
+        name: "Onur",
+        surname: "Alp",
+        age: 22,
+        address: "Address Test 1"
+    },
+    {
+        name: "Other",
+        surname: "Other Surname",
+        age: 25,
+        address: "Address Test 2"
+    }
+];
--- index.ts
+++ index.ts
@@ -1,16 +1,21 @@
 
 // type userInfoTypes = name:string, surname:string, age:number;
 
+type ageType = number | string;
+
 interface userInfoTypes {
     name: string,
     surname:string,
-    age:number
+    age:ageType,
+    gender?: "Man" | "Woman",
+    address?: string
 }
 
 let userInfo: userInfoTypes = {
     name: "Onur",
     surname: "Alp",
-    age: 15
+    age: 22,
+    gender: "Man"
 }
 
 
@@ -19,3 +24,19 @@ type MathTypes = (x: number, y: number) => number;
 const basicMath: MathTypes = (x, y) => x + y;
 
 const result = basicMath(5,5);
+
+
+const users: userInfoTypes[] = [
+    {
+        name: "Onur",
+        surname: "Alp",
+        age: 22,
+        address: "Address Test 1",
+    },
+    {
+        name: "Other",
+        surname: "Other Surname",
+        age: 25,
+        address: "Address Test 2"
+    }
+]
